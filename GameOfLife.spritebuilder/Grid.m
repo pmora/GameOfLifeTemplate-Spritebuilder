ad89e4720130ea94904c480e7ce2d47ab0ec1f98
pmora <pau.mora@icloud.com>
2015-01-02 16:26:04 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -174,7 +174,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = TRUE;
-                numAlive += 1*(currentCreature.livingNeighbors - 1);
+                numAlive ++;
             }
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >=4)
             {
